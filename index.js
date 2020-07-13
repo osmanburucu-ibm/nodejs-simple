dfcb37734d5a24b83da3429908671ab57f32da19
Osman Burucu <58933456+osmanburucu-ibm@users.noreply.github.com>
2020-07-13 14:26:40 +0300

    new text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.end('Hello World!\n');
+    res.end('Hello BRAVE NEW World!\n');
 });
 
 server.listen(port, () => {
